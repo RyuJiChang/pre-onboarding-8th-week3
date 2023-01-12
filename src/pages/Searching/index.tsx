bc0492b0db1ddc35aeefacd5b7cec40970c1df4a
RyuJiChang <rgc0582@gmail.com>
2023-01-12 11:32:40 +0900

    feat: 글자 및 input 위치 잡기

--- src/pages/Searching/index.tsx
+++ src/pages/Searching/index.tsx
@@ -1,7 +1,14 @@
-import { Container } from './styles.Searching';
+import { Container, Header, DropDown } from './styles.Searching';
 
 function Searching() {
-  return <Container>Searching</Container>;
+  return (
+    <Container>
+      <Header>국내 모든 임상시험 검색하고 온라인으로 참여하기</Header>
+      {/* <SerchingContaine></SerchingContaine> */}
+      {/* <KeywordSearching></KeywordSearching> */}
+      <DropDown>123</DropDown>
+    </Container>
+  );
 }
 
 export default Searching;
--- src/pages/Searching/styles.Searching.ts
+++ src/pages/Searching/styles.Searching.ts
@@ -2,9 +2,25 @@ import styled from 'styled-components';
 import { THEME } from '../../constants';
 
 const Container = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: ${THEME.BackGroundColor};
 `;
 
-export { Container };
+const Header = styled.h1`
+  width: 12em;
+  margin-top: 4em;
+  text-align: center;
+  line-height: 1.7em;
+`;
+
+const DropDown = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-top: 2em;
+  background-color: gray;
+`;
+export { Container, Header, DropDown };
